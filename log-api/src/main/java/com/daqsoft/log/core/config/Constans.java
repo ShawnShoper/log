2c2e3faac746747b949cf96d463173db73f5ffe5
谢浩 <xieh@daqsoft.com>
2017-10-18 14:16:59 +0800

    添加Major Version

--- log-api/src/main/java/com/daqsoft/log/core/config/Constans.java
+++ log-api/src/main/java/com/daqsoft/log/core/config/Constans.java
@@ -1,14 +1,20 @@
 package com.daqsoft.log.core.config;
 
 public class Constans {
+    //主版本号
+    public static final String MAJORVERSION = "1";
     //默认的logtopic
     public static final String LOGTOPIC = "sys-log";
     //Property
     public static final String PROPERTY = "property";
     public static final String FNAME = "fname";
     public static final String LINE = "line";
-
+    public static final String PERCENT = "%";
+    public static final String SPACE = " ";
+    public static final String EMPTY = "";
+    public static final String DASH = "-";
     public static final String INFO = "INFO";
+    public static final String NEWLINE = "\r\n";
     //结束行
     public static final String OVER = "\001OVER\001";
     public static final String WARN = "WARN";
@@ -16,6 +22,7 @@ public class Constans {
     public static final String ERROR = "ERROR";
     //默认分隔符
     public static final String PLACEHOLDER = "\001";
+    public static final String PLACEHOLDER2 = "\002";
     public static final String TYPE_JSON = "JSON";
     public static final String TYPE_XML = "XML";
     public static final String TYPE_STRING = "STRING";
